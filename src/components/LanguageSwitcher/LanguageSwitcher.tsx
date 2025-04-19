f1593c3c0c7380eca139a7385821d8bce05b4ae0
Dzmitry_Yarash <dzmitry_yarash@epam.com>
2025-04-19 19:51:35 +0300

    fix: fixed language switcher and tests

--- src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,24 +1,28 @@
 'use client';
 
 import { useLocale } from 'next-intl';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { JSX } from 'react';
 
 const LanguageSwitcher = (): JSX.Element => {
   const locale = useLocale();
   const router = useRouter();
   const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   const newLocale = locale === 'en' ? 'ru' : 'en';
 
   const switchLocale = (): void => {
+    const queryString = searchParams.toString();
+    const query = queryString ? `?${queryString}` : '';
+
     if (pathname === `/${locale}` || pathname === `/${locale}/`) {
-      router.push(`/${newLocale}`);
+      router.push(`/${newLocale}${query}`);
       return;
     }
 
     const newPathname = pathname.replace(`/${locale}/`, `/${newLocale}/`);
-    router.push(newPathname);
+    router.push(`${newPathname}${query}`);
   };
 
   return (
